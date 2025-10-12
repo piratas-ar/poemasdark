efdc06b7cb7bd1842db4a6c6c81645a777fb2036
gabeQA <109978987+gabeQA@users.noreply.github.com>
2025-10-12 16:49:25 -0300

    Update autores.ts
    mas poemas

--- lib/autores.ts
+++ lib/autores.ts
@@ -9,7 +9,7 @@ export interface Quote {
 }
 
 export const autores: Quote[] = [
-  { titulo: `Fuga de muerte` texto: `Fuga de muerte
+  { titulo: `Fuga de muerte`, texto: `Fuga de muerte
   
   Negra leche del alba la bebemos por la tarde
 la bebemos al mediodía y en la mañana la bebemos de noche
@@ -59,15 +59,128 @@ tu pelo dorado Margarete
 tu pelo cenizo Sulamith`, autor: `Judío`, nombre: `Paul Celan
 1920-1970
 Fue un poeta de habla alemana del siglo XX. Nació en Chernivtsi del entonces reino de Rumanía en 1920 en una familia judía. Durante la segunda guerra mundial estuvo en un campo de trabajo forzado y tras escapar se estableció en París, donde murió en 1970.`, año: `1970 - 2002`, bio:` `   },
-  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
-  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
-  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
-  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
-  { texto: `Si quieres acariciarme, yo no me opondría
+  { titulo: `Recuerdos de Polonia` ,texto: `Recuerdos de Polonia
+
+En Varsovia, una muchacha hablaba así:
+si quieres acariciarme, yo no me opondría;
+si quieres besarme, puedes hacerlo
+te permitiría que me desnudes los senos.
+Pero debes saber que a papá lo fusilaron los alemanes
+y a un hermano mío lo quemaron en los hornos.
+ 
+Si quieres acariciarme, yo no me opondría
 pero debes saber que todos estos muertos
 están en mí
 y yo toda, toda soy de ceniza.
-Bésame, pero que no te sepa amarga.`, autor: `Judío`, nombre: `Geo Bogda - Recuerdos de Polonia` },
+Bésame, pero que no te sepa amarga.
+
+
+II
+ 
+  
+En Cracovia, una muchacha hablaba así:
+si quieres puedes abrazarme
+si quieres puedes acariciarme los senos
+pero no me compres abalorios, nunca.
+Tenía trece años cuando los alemanes
+ahorcaron a mamá, de un árbol en la calle.
+ 
+Si quieres podemos atravesar nadando el Vístula
+pero no me digas que tengo el cuello blanco y bello
+
+y no me compres abalorios, nunca.
+
+ 
+
+ `, autor: `Judío`, nombre: `Geo Bogda
+
+(Rumanía, 1909 – Ucrania, 1993)
+
+Fue un teórico, poeta y periodista rumano de vanguardia, conocido por sus convicciones políticas comunistas y de izquierda. De joven en el período de entreguerras, era conocido como un rebelde y era uno de los surrealistas rumanos más influyentes. Varios de sus polémicos poemas lo llevaron dos veces a ser encarcelado por obscenidad, y lo vieron participar en el conflicto entre jóvenes y viejos escritores rumanos, así como en el enfrentamiento entre la vanguardia y la extrema derecha. En una etapa posterior, Bogza ganó renombre por sus numerosos y logrados reportajes, siendo uno de los primeros en cultivar el género en la literatura rumana, y usándolo como un lugar para la crítica social. `, año: `Rumanía, 1909 – Ucrania, 1993`, bio:`Fue un teórico, poeta y periodista rumano de vanguardia, conocido por sus convicciones políticas comunistas y de izquierda. De joven en el período de entreguerras, era conocido como un rebelde y era uno de los surrealistas rumanos más influyentes. Varios de sus polémicos poemas lo llevaron dos veces a ser encarcelado por obscenidad, y lo vieron participar en el conflicto entre jóvenes y viejos escritores rumanos, así como en el enfrentamiento entre la vanguardia y la extrema derecha. En una etapa posterior, Bogza ganó renombre por sus numerosos y logrados reportajes, siendo uno de los primeros en cultivar el género en la literatura rumana, y usándolo como un lugar para la crítica social. `   },
+  { titulo: `Raíz` ,texto: `Raíz
+  
+Con fuerza se desliza la raíz, 
+bebe de la lluvia, vive bajo tierra,
+sus sueños son limpios como la nieve.
+
+De la tierra brota y rompe el suelo,
+a escondidas se arrastra la astuta raíz
+levantando sus brazos, como una soga.
+
+En el brazo de la raíz un gusano se acuesta,
+y un gusano se sienta a los pies de la raíz,
+el suyo es un mundo podrido de gusanos.
+
+Mas la raíz resiste en la hondura del suelo,
+alejada de todo
+salvo de la rama asceta que aún luce verdor.
+
+Esto es lo que nutre, esto es lo que ama,
+el perfume que sopla hacia la altura,
+los fragancias maduras que alimentan el cielo.
+
+Yo soy raíz ahora,
+y vivo entre gusanos.
+Este poema es de allí.
+
+Yo era una flor. Ahora soy raíz.
+Oscura, la tierra me cubre por entero,
+Mi destino se ha cumplido.
+Sobre mi cabeza, una sierra llora.
+`, autor: `Judío`, nombre: `Miklós Radnóti 
+(Budapest, 5 de mayo de 1909 - Abda, 9 de noviembre de 1944)
+Miklós Radnóti fue un poeta húngaro judío del siglo XX, considerado uno de los líricos más importantes de la primera mitad de ese siglo. Su obra, que evolucionó desde un enfoque más optimista hasta uno de profunda aprehensión, reflejó el horror y el sufrimiento del Holocausto. Fue asesinado por los nazis durante una marcha forzada en 1944, aunque el famoso "Cuaderno de Bor" con sus últimos poemas fue encontrado en su abrigo tras ser exhumado.  `, año: `(Budapest, 5 de mayo de 1909 - Abda, 9 de noviembre de 1944)`, bio:` Miklós Radnóti fue un poeta húngaro judío del siglo XX, considerado uno de los líricos más importantes de la primera mitad de ese siglo. Su obra, que evolucionó desde un enfoque más optimista hasta uno de profunda aprehensión, reflejó el horror y el sufrimiento del Holocausto. Fue asesinado por los nazis durante una marcha forzada en 1944, aunque el famoso "Cuaderno de Bor" con sus últimos poemas fue encontrado en su abrigo tras ser exhumado.  
+ `   },
+  { titulo: `La Recherche` ,texto: `La Recherche
+
+
+Recuerda también las viejas y suaves tardes
+Y sus brillantes mesas coronadas por los poetas y sus jóvenes esposas.
+¿Dónde queda el barro del recuerdo en que te deslizas, dónde está la noche en la que los exuberantes amigos bebían alegremente el vino natal
+que brillaba en sus ojos bellísimos?.
+
+Los versos nadaban alrededor del resplandor de las lámparas.
+Y los adjetivos verdes y brillantes se balanceaban sobre la cresta espumosa de su música, y los muertos estaban vivos, y los prisioneros en sus casas, los poemas de los viejos amigos cuyos corazones se perdieron bajo la tierra en España, Flandes y Ucrania.
+
+Algunos se lanzaron hacia el frente, apretando sus dientes contra el fuego, y lucharon porque nada pudieron hacer para evitarlo,
+mientras dormía la alarma de un mundo adormecido y sucio,
+Bajo la cubierta de la noche, recordando los viejos hogares del pasado,
+sus cuevas e islas tranquilas, alejadas de este tiempo.
+ Algunos de ellos viajaron indefensos en vagones sellados de ganado a lugares donde se quedaron aturdidos y rígidos, esperando desarmados en los helados campos de minas, otros marcharon voluntariamente y silenciosos con un arma en la mano al lugar que escogieron, donde estaba su lucha y ahora el ángel de la libertad guarda sus sueños en la noche.
+Algunos, no importa ya. ¿De dónde vienen los vinos sabios?
+Llegaron los invasores y se multiplicaron. Y se multiplicaron las arrugas alrededor de las sonrisas de sus labios y de sus ojos de las mujeres. Y se cansaron de las hadas las muchachas en medio del silencio de la guerra.
+
+Dónde está la noche, la taberna, los tres, debajo de esta mesa.
+Y quienes viven aún, dónde están cayendo ahora los que combaten.
+Mi corazón oye sus voces, mis manos se anudan a sus manos,
+su valor y sus torsos se despliegan ante mi
+-prisionero silencioso- en las terribles montañas de Serbia.
+
+Dónde está la noche. Esa noche ya nunca volverá.
+La Muerte lo mira todo desde otra perspectiva.
+Todavía se sientan a la mesa, se esconden en la sonrisa de las mujeres.
+Y volverán a beber de nuevo en nuestras copas, los amigos aún sin sepultar en los bosques remotos, dormidos ya en pastos extraños…`, autor: `Judío`, nombre: `Miklós Radnóti 
+(Budapest, 5 de mayo de 1909 - Abda, 9 de noviembre de 1944)
+Miklós Radnóti fue un poeta húngaro judío del siglo XX, considerado uno de los líricos más importantes de la primera mitad de ese siglo. Su obra, que evolucionó desde un enfoque más optimista hasta uno de profunda aprehensión, reflejó el horror y el sufrimiento del Holocausto. Fue asesinado por los nazis durante una marcha forzada en 1944, aunque el famoso "Cuaderno de Bor" con sus últimos poemas fue encontrado en su abrigo tras ser exhumado.  `, año: `(Budapest, 5 de mayo de 1909 - Abda, 9 de noviembre de 1944)`, bio:` Miklós Radnóti fue un poeta húngaro judío del siglo XX, considerado uno de los líricos más importantes de la primera mitad de ese siglo. Su obra, que evolucionó desde un enfoque más optimista hasta uno de profunda aprehensión, reflejó el horror y el sufrimiento del Holocausto. Fue asesinado por los nazis durante una marcha forzada en 1944, aunque el famoso "Cuaderno de Bor" con sus últimos poemas fue encontrado en su abrigo tras ser exhumado.  
+ `   },
+  { titulo: `El ropero vacio ` ,texto: `El ropero vacio 
+El viento de setiembre repite el último pedido
+de mi hermano Iukev:
+ —Voy a esconderme, Iashek, en tu casa,
+en el ropero vacío;
+solo necesitas traerme alguna vez
+un poco de agua,
+un trozo de pan
+para sobrevivir.
+Pero Iashek, nuestro vecino polaco,
+guardó silencio. 
+De los árboles caen muertas, junto con las hojas amarillas,
+las últimas palabras de mi hermano.
+Todos los roperos vacíos del mundo
+tienen ahora sus puertas abiertas de par en par,
+y esperan que mi hermano venga a beber agua y a comer pan.`, autor: `Judío`, nombre: `Reizl Zhijlinsky`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` ,texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` ,texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
   { texto: `De los árboles caen muertas, junto con las hojas amarillas,
 
 las últimas palabras de mi hermano.`, autor: `Judío`, nombre: `Reizl Zhijlinsky - El ropero vacío`  },
