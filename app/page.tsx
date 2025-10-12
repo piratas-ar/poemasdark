4c060e6fd913c581e07b7865a1dd38e2cec3118d
gabeQA <109978987+gabeQA@users.noreply.github.com>
2025-10-12 15:54:25 -0300

    Update page.tsx
    intento de alinear poemas a la izquierda

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function AuthorQuiz() {
             ) : (
               <div>
                 <div className="bg-light rounded p-4 mb-4 d-flex" style={{ minHeight: '150px' }}>
-                  <pre className="fs-5 fst-italic text-center mb-0">
+                  <pre className="fs-5 fst-italic text-left mb-0">
                     {currentQuote.texto}
                   </pre>
                 </div>
