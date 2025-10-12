93e8fd96f9749d38555b8703a60626950d27fe1e
gabeQA <109978987+gabeQA@users.noreply.github.com>
2025-10-12 15:59:02 -0300

    Update autores.ts
    Poema nuevo y agregar mas detalles como titulo año bio etc

--- lib/autores.ts
+++ lib/autores.ts
@@ -1,11 +1,68 @@
 export type Ethnicity = 'Judío' | 'Palestino';
 export interface Quote {
+  titulo: string;
   texto: string;
   autor: Ethnicity
+  año: string;
+  bio: string;
   nombre: string;
 }
 
 export const autores: Quote[] = [
+  { titulo: `Fuga de muerte` texto: `Fuga de muerte
+  
+  Negra leche del alba la bebemos por la tarde
+la bebemos al mediodía y en la mañana la bebemos de noche
+bebemos y bebemos
+cavamos una tumba en los aires donde no hay opresión
+Un hombre habita en la casa juega con las serpientes escribe
+escribe cuando oscurece a Alemania tu pelo dorado Margarete
+escribe eso y sale frente a la casa y brillan las estrellas silba a sus perros
+silba a sus judíos caven una tumba en la tierra
+nos ordena vamos toquen para el baile
+
+
+Negra leche del alba te bebemos de noche
+te bebemos en la mañana y al mediodía te bebemos por la tarde
+bebemos y bebemos
+Un hombre habita en la casa juega con las  serpientes escribe
+escribe cuando oscurece a Alemania tu pelo dorado Margarete
+Tu pelo cenizo Sulamith cavamos una tumba en los aires donde no hay opresión
+
+
+Grita ustedes caven más hondo en la tierra los demás canten y toquen
+empuña el fierro en el cinto lo blande sus ojos son azules
+ustedes entierren las palas más hondo los demás sigan tocando para el baile
+
+
+Negra leche del alba te bebemos de noche
+te bebemos al mediodía y en la mañana te bebemos por la tarde
+bebemos y bebemos
+un hombre habita en la casa tu pelo dorado Margarete
+tu pelo cenizo Sulamith juega con las serpientes
+Grita toquen más dulcemente la muerte la muerte es un maestro de Alemania
+grita tañan más sombríamente los violines para que asciendan cual humo en el aire
+para que tengan una tumba en las nubes donde no hay opresión
+
+
+Negra leche del alba te bebemos de noche
+te bebemos al mediodía la muerte es un maestro de Alemania
+te bebemos por la tarde y en la mañana bebemos y bebemos
+la muerte es un maestro de Alemania su ojo es azul
+te acierta con bala de plomo te acierta preciso
+un hombre habita en la casa tu pelo dorado Margarete
+atiza sus perros contra nosotros nos regala una tumba en el aire
+juega con las serpientes y sueña la muerte es un maestro de Alemania
+
+
+tu pelo dorado Margarete
+tu pelo cenizo Sulamith`, autor: `Judío`, nombre: `Paul Celan
+1920-1970
+Fue un poeta de habla alemana del siglo XX. Nació en Chernivtsi del entonces reino de Rumanía en 1920 en una familia judía. Durante la segunda guerra mundial estuvo en un campo de trabajo forzado y tras escapar se estableció en París, donde murió en 1970.`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
+  { titulo: `Ejemplo` texto: `poema ejemplo`, autor: `Palestino`, nombre: `josé pérez`, año: `1970 - 2002`, bio:` `   },
   { texto: `Si quieres acariciarme, yo no me opondría
 pero debes saber que todos estos muertos
 están en mí
@@ -20,8 +77,7 @@ ayer. Si asesinas serpientes
 no hubieran alborotado todos los caminos, cavando tumbas para
 mis gentes y mi pueblo, sembrando muerte y fuego.
 Si no hubiera regado la derrota la tierra de mi patria
-con piedras vergonzosas, injuriantes. Si este corazón que tú cono-
-ces
+con piedras vergonzosas, injuriantes. Si este corazón que tú conoces
 fuera el mismo que ayer,
 y no sangrase por la puñalada.
 Si hoy, amigo mío, como ayer, pudiera envanecerme de mi gente,
